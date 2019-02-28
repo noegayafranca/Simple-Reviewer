13102c07973401cc0ecada0e9da0d5ff8a8e3bec
cesar-neo <12345fern2017@gmail.com>
2019-02-28 18:12:01 +0800

    first commit

---
+++ Reviewer v.3/Reviewer v.3/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Reviewer_v._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.save = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menucombobox = new System.Windows.Forms.ToolStripComboBox();
+            this.review = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.paste = new System.Windows.Forms.ToolStripMenuItem();
+            this.history = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbquestion = new System.Windows.Forms.Label();
+            this.lbfolder = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Purple;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.save,
+            this.folderToolStripMenuItem,
+            this.menucombobox,
+            this.review,
+            this.clearToolStripMenuItem,
+            this.paste,
+            this.history});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.ShowItemToolTips = true;
+            this.menuStrip1.Size = new System.Drawing.Size(797, 27);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // save
+            // 
+            this.save.ForeColor = System.Drawing.Color.White;
+            this.save.Image = ((System.Drawing.Image)(resources.GetObject("save.Image")));
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(59, 23);
+            this.save.Text = "Save";
+            this.save.ToolTipText = "Save";
+            this.save.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // folderToolStripMenuItem
+            // 
+            this.folderToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.folderToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("folderToolStripMenuItem.Image")));
+            this.folderToolStripMenuItem.Name = "folderToolStripMenuItem";
+            this.folderToolStripMenuItem.Size = new System.Drawing.Size(68, 23);
+            this.folderToolStripMenuItem.Text = "Folder";
+            this.folderToolStripMenuItem.ToolTipText = "Select Folder";
+            this.folderToolStripMenuItem.Click += new System.EventHandler(this.folderToolStripMenuItem_Click);
+            // 
+            // menucombobox
+            // 
+            this.menucombobox.ForeColor = System.Drawing.Color.Black;
+            this.menucombobox.Name = "menucombobox";
+            this.menucombobox.Size = new System.Drawing.Size(220, 23);
+            this.menucombobox.SelectedIndexChanged += new System.EventHandler(this.menucombobox_SelectedIndexChanged);
+            // 
+            // review
+            // 
+            this.review.AutoToolTip = true;
+            this.review.ForeColor = System.Drawing.Color.White;
+            this.review.Image = ((System.Drawing.Image)(resources.GetObject("review.Image")));
+            this.review.Name = "review";
+            this.review.Size = new System.Drawing.Size(56, 23);
+            this.review.Text = "OFF";
+            this.review.ToolTipText = "Review Mode";
+            this.review.Click += new System.EventHandler(this.reviewToolStripMenuItem_Click);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.clearToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("clearToolStripMenuItem.Image")));
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(62, 23);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // paste
+            // 
+            this.paste.ForeColor = System.Drawing.Color.White;
+            this.paste.Image = ((System.Drawing.Image)(resources.GetObject("paste.Image")));
+            this.paste.Name = "paste";
+            this.paste.Size = new System.Drawing.Size(56, 23);
+            this.paste.Text = "OFF";
+            this.paste.ToolTipText = "Paste Mode";
+            this.paste.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // history
+            // 
+            this.history.ForeColor = System.Drawing.Color.White;
+            this.history.Name = "history";
+            this.history.Size = new System.Drawing.Size(57, 23);
+            this.history.Text = "History";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treeView1.Location = new System.Drawing.Point(0, 27);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(153, 377);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.NodeMouseHover += new System.Windows.Forms.TreeNodeMouseHoverEventHandler(this.treeView1_NodeMouseHover);
+            this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Yellow;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(153, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(644, 27);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseClick);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(153, 54);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(644, 350);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.lbquestion);
+            this.panel1.Controls.Add(this.lbfolder);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(153, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(644, 350);
+            this.panel1.TabIndex = 4;
+            this.panel1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(6, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Show Answer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbquestion
+            // 
+            this.lbquestion.AutoSize = true;
+            this.lbquestion.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbquestion.Location = new System.Drawing.Point(13, 66);
+            this.lbquestion.Name = "lbquestion";
+            this.lbquestion.Size = new System.Drawing.Size(68, 19);
+            this.lbquestion.TabIndex = 1;
+            this.lbquestion.Text = "Question";
+            // 
+            // lbfolder
+            // 
+            this.lbfolder.AutoSize = true;
+            this.lbfolder.Location = new System.Drawing.Point(48, 286);
+            this.lbfolder.Name = "lbfolder";
+            this.lbfolder.Size = new System.Drawing.Size(33, 13);
+            this.lbfolder.TabIndex = 0;
+            this.lbfolder.Text = "folder";
+            this.lbfolder.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 404);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Reviewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem save;
+        private System.Windows.Forms.ToolStripMenuItem folderToolStripMenuItem;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbfolder;
+        private System.Windows.Forms.ToolStripComboBox menucombobox;
+        private System.Windows.Forms.Label lbquestion;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem review;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem paste;
+        private System.Windows.Forms.ToolStripMenuItem history;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Reviewer v.3/Reviewer v.3/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reviewer_v._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void folderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if(fbd.ShowDialog() == DialogResult.OK)
+            {
+                lbfolder.Text = fbd.SelectedPath;
+                menucombobox.Items.Clear();
+                string[] files = Directory.GetFiles(fbd.SelectedPath);
+                string[] dirs = Directory.GetDirectories(fbd.SelectedPath);
+                Properties.Settings.Default.foldersettings = lbfolder.Text;
+                Properties.Settings.Default.Save();
+
+                foreach (string item2 in dirs)
+                {
+                    FileInfo f = new FileInfo(item2);
+
+                    menucombobox.Items.Add(f.Name);
+
+                }
+            }
+        }
+
+        public void ShowAllFolders()
+        {
+           
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(menucombobox.Text == "")
+            {
+                MsgboxError("Select folder where you want to save your data.");
+                menucombobox.Focus();
+            }
+            else if(textBox1.Text == "")
+            {
+                MsgboxError("Enter filename.");
+                textBox1.Focus();
+            }
+            else if(richTextBox1.Text == "")
+            {
+                MsgboxError("Field is empty, nothing to save.");
+                richTextBox1.Focus();
+            }
+            else
+            {
+                if(review.Text == "ON")
+                {
+                    SaveFile();
+                    MsgboxInfo("Successfully Updated.");
+                    richTextBox1.Clear();
+                    textBox1.Clear();
+                }
+                else
+                {
+                    if (System.IO.File.Exists(lbfolder.Text + "\\" + menucombobox.Text + "\\" + textBox1.Text + ".txt"))
+                    {
+                        MsgboxError("File already exist.");
+                        textBox1.Focus();
+                    }
+                    else
+                    {
+                        SaveFile();
+                        MsgboxInfo("Successfully Saved");
+                        treeView1.Nodes.Add(textBox1.Text + ".txt");
+                        richTextBox1.Clear();
+                        Properties.Settings.Default.lastsave = textBox1.Text;
+                        Properties.Settings.Default.Save();
+                        history.Text = Properties.Settings.Default.lastsave;
+                        textBox1.Clear();                   
+                    }
+                }
+            }
+        }
+
+        public void SaveFile()
+        {
+            try
+            {
+                StreamWriter str = new StreamWriter(lbfolder.Text + "\\" + menucombobox.Text + "\\" + textBox1.Text + ".txt");
+                str.WriteLine(richTextBox1.Text);
+                str.Close();
+            }
+            catch(Exception ex)
+            {
+                MsgboxError(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Hide();
+            //int i;
+            //for (i = 1; i < 100; i++)
+            //{
+            //    richTextBox1.Text += i + "\n";
+            //}
+        }
+
+        public void MsgboxError(string message)
+        {
+            MessageBox.Show(message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public void MsgboxInfo(string message)
+        {
+            MessageBox.Show(message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void treeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+           if(review.Text == "ON")
+            {
+                treeView1.SelectedNode.Remove();
+                textBox1.Text = e.Node.Text;
+                string text = textBox1.Text;
+                textBox1.Text = text.Replace(".txt", "");
+                lbquestion.Text = text.Replace(".txt", " ?");
+                StreamReader str = new StreamReader(lbfolder.Text + "\\" + menucombobox.Text + "\\" + e.Node.Text);
+                richTextBox1.Text = str.ReadToEnd();
+                str.Close();
+                save.Text = "UPDATE";
+                panel1.Show();
+            }
+            else
+            {
+
+            }
+        }
+
+        private void reviewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(review.Text == "OFF")
+            {
+                review.Text = "ON";
+            }
+            else
+            {
+                review.Text = "OFF";
+                save.Text = "Save";
+            }
+        }
+
+        private void menucombobox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Clear();
+            DirectoryInfo d = new DirectoryInfo(lbfolder.Text+"\\"+menucombobox.Text);
+            FileInfo[] Files = d.GetFiles("*.txt");       
+            foreach (FileInfo file in Files)
+            {          
+                treeView1.Nodes.Add(file.Name);
+            }
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            textBox1.Clear();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(paste.Text == "OFF")
+            {
+                paste.Text = "On";
+            }
+            else
+            {
+                paste.Text = "OFF";
+            }
+        }
+
+        private void textBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (paste.Text == "On")
+            {
+                textBox1.Paste();
+                timer1.Enabled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            history.Text = Properties.Settings.Default.lastsave;
+
+            lbfolder.Text = Properties.Settings.Default.foldersettings;
+
+            
+            try
+            {
+                string[] dirs = Directory.GetDirectories(lbfolder.Text);
+                foreach (string item2 in dirs)
+                {
+                    FileInfo f = new FileInfo(item2);
+
+                    menucombobox.Items.Add(f.Name);
+
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string str = textBox1.Text;
+            textBox1.Text = str.Replace("()", "");          
+            timer1.Enabled = false;
+        }
+
+        private void treeView1_NodeMouseHover(object sender, TreeNodeMouseHoverEventArgs e)
+        {
+            e.Node.ToolTipText = "";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if(review.Text == "ON")
+            {
+
+            }
+            else
+            {
+                string full = lbfolder.Text + "\\" + menucombobox.Text + "\\" + textBox1.Text + ".txt";
+                if (System.IO.File.Exists(full))
+                {
+                    MsgboxError("Filename Already Exist.");
+                }
+            }
+        }
+    }
+}
